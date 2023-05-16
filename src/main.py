663ee43bcff3fbe12ed6d95810fa321a4fa4e242
guerlain BLANCHARD <guerlain.blanchard@gmail.com>
2023-05-16 17:12:13 +0900

    [Add] model save on class destruction

--- src/main.py
+++ src/main.py
@@ -6,9 +6,12 @@ from tensorflow import keras
 import numpy as np
 
 class algorithm:
-    def __init__(self, model) -> None:
+    def __init__(self, model):
         self.model_path = '../saved_model' + model
         if (os.path.exists(self.model_path)):
             self.model = tf.keras.models.load_model(self.model_path)
         else:
             self.model = tf.keras.Sequential()
+    
+    def __del__(self):
+        self.model.save(self.model_path)
