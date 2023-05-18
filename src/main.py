637cbc5d2babbf5f05dd2c22b66a46794f5c892f
guerlain BLANCHARD <guerlain.blanchard@gmail.com>
2023-05-18 16:26:07 +0900

    [Add] complete handleMissing function

--- src/main.py
+++ src/main.py
@@ -21,5 +21,25 @@ class algorithm:
     def __str__(self, predictSet):
         return(self.model.predict(predictSet))
     
+    def handleMissing(self, dataSet: pd.DataFrame) -> pd.DataFrame:
+        if (dataSet.isnull().sum() == 0):
+            return (dataSet)
+        print("The given file has missing values, please enter ow you wish to handle them:")
+        print("\t1 - Drop the values")
+        print("\t2 - Replace with the most frequent values")
+        print("\t3 - Replace with the most average values")
+        option = input(">> ")
+        if option == '1':
+            return (dataSet.dropna())
+        elif option == '2':
+            return (dataSet.fillna(dataSet.mode().iloc[0]))
+        elif option == '3':
+            return (dataSet.fillna(dataSet.mean()))
+        elif option == 'EXIT':
+            exit()
+        else:
+            print('\033[93m' + "Incorect value, please enter a correct value or type EXIT to stop" + '\033[0m')
+            return(self.handleMissing(dataSet))
+
     def train(self, trainingSet):
         trainingData = pd.read_csv(trainingSet)
