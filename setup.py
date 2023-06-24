a7d7e86e192c274cd52055180487d841e657c629
guerlain BLANCHARD <guerlain.blanchard@gmail.com>
2023-06-24 23:48:41 +0200

    [Add] continuation of the setup.py file

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup
 
-setup()
+setup_info = dict(
+    name="tf_project",
+    author="Guerlain BLANCHARD",
+    author_email="guerlain.blanchard@gmail.com",
+
+)
+
+setup(**setup_info)
